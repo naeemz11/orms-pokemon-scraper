066716df84f4e1ab7d1ea551e971c64f2e65e79d
your_name <your_email@example.com>
2021-03-03 13:08:36 -0500

    commit tree bdbb4c4c59fafbb663f9fb344d072489a1a469cc

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,45 @@
 class Pokemon
-end
+
+    attr_reader :id, :name, :type, :hp, :db
+  
+
+  def initialize (id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+#   def self.save(name, type, db)
+#     sql = <<-SQL
+#       INSERT INTO pokemon (name, type) VALUES (?, ?);
+#     SQL
+    
+#     db.execute(sql, [name, type])
+    
+#   end
+
+  def self.save(name, type, db)
+   db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+
+  def self.find(id, db)
+    sql = <<-SQL
+    
+    SELECT * FROM pokemon 
+    WHERE id = ?
+    
+    SQL
+   
+    #binding.pry 
+    pokemon = db.execute(sql, [id]).flatten
+    name = pokemon[1]
+    type = pokemon[2]
+    Pokemon.new(id: id, name: name, type: type, db: db)
+
+
+  end
+
+end 
+  
